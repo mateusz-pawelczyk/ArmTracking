bffe529d22aef48e6127a2be0b57503cc5ea49f8
mateuszpawelczyk <mateuszpawelczyk2003@gmail.com>
2024-08-12 21:53:49 +0200

    mediapipe only solution is pretty good hihi

--- track_whole_arm.py
+++ track_whole_arm.py
@@ -2,111 +2,139 @@ import pyrealsense2 as rs
 import numpy as np
 import cv2
 import mediapipe as mp
+import csv
+import pandas as pd
 
 # Configure depth and color streams
 pipeline = rs.pipeline()
 config = rs.config()
-
-# Enable both depth and color streams
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
-
-# Start streaming
-pipeline.start(config)
+profile = pipeline.start(config)
 
 # Initialize MediaPipe
 mp_hands = mp.solutions.hands
 mp_pose = mp.solutions.pose
-hands = mp_hands.Hands(min_detection_confidence=0.55, min_tracking_confidence=0.5)
-pose = mp_pose.Pose(min_detection_confidence=0.55, min_tracking_confidence=0.55)
+hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7, min_tracking_confidence=0.7)
+pose = mp_pose.Pose(min_detection_confidence=0.7, min_tracking_confidence=0.7)
 mp_drawing = mp.solutions.drawing_utils
 
-def norm_vector_to_other_vector(v_to_norm_on, v_to_norm):
-    print(f"v_to_norm_on: {v_to_norm_on}")
-    print(f"v_to_norm: {v_to_norm}")
-    print(f"v_to_norm_on / np.linalg.norm(v_to_norm_on): {v_to_norm_on / np.linalg.norm(v_to_norm_on)}")
-    print((v_to_norm[0],v_to_norm[1], (v_to_norm_on / np.linalg.norm(v_to_norm_on))@v_to_norm))
-    return (v_to_norm[0],v_to_norm[1], (v_to_norm_on / np.linalg.norm(v_to_norm_on))@v_to_norm)
+AMOUNT_OF_LANDMARKS = 23 # 2*arm + 20 Finger
+ROW_CSV = [0] * (AMOUNT_OF_LANDMARKS * 3)
 
-VECTOR_FOR_CALIBRATION = ()
+FRAME = 0
 
+def start_recording():
+    global csvfile, csv_writer, recording
+    csvfile = open('recorded_data.csv', 'w', newline='')
+    csv_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    header = [[f'X_{id}', f'Y_{id}', f'Z_{id}'] for id in range(AMOUNT_OF_LANDMARKS)]
+    header_final = ["FRAME"]
+    for i in header:
+        header_final.extend(i)
+    csv_writer.writerow(header_final)
+    recording = True
+    print("Recording started.")
+
+def stop_recording():
+    global csvfile, recording
+    csvfile.close()
+    recording = False
+    print("Recording stopped.")
+
+def point_in_screen(point_x,point_y):
+    return 0<= point_x <=1 and 0<= point_y <= 1
+
+recording = False
+
+WRIST_DETPH = None
 
 try:
     while True:
-        # Wait for a coherent pair of frames: depth and color
-        frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
-        # Convert images to numpy arrays
-        depth_image = np.asanyarray(depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
-        y_mid_of_frame = int(0.5 * color_image.shape[0])
-        x_mid_of_frame = int(0.5 * color_image.shape[1])
-        depth = depth_frame.get_distance(x_mid_of_frame, y_mid_of_frame)
-        VECTOR_FOR_CALIBRATION = (x_mid_of_frame, y_mid_of_frame, depth)
-        if depth != 0:
-            break
-
-    while True:
-        # Wait for a coherent pair of frames: depth and color
         frames = pipeline.wait_for_frames()
         depth_frame = frames.get_depth_frame()
         color_frame = frames.get_color_frame()
         if not depth_frame or not color_frame:
             continue
+        FRAME +=1
 
-        # Convert images to numpy arrays
         depth_image = np.asanyarray(depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
-
-        # Convert the BGR image to RGB before processing with MediaPipe
         color_image_rgb = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
         hand_results = hands.process(color_image_rgb)
         pose_results = pose.process(color_image_rgb)
 
-         # Draw hand landmarks
+        # Visualize and record arm landmarks with depth
+        if pose_results.pose_landmarks:
+            arm_landmarks = [mp_pose.PoseLandmark.RIGHT_SHOULDER, mp_pose.PoseLandmark.RIGHT_ELBOW, mp_pose.PoseLandmark.RIGHT_WRIST]
+            for id, joint in enumerate(arm_landmarks):
+                if joint == mp_pose.PoseLandmark.RIGHT_WRIST:
+                    WRIST_DETPH = landmark.z
+                    continue
+                    
+                    
+                landmark = pose_results.pose_landmarks.landmark[joint]
+                x, y = int(landmark.x * color_image.shape[1]), int(landmark.y * color_image.shape[0])
+                
+                if point_in_screen(landmark.x, landmark.y):
+
+                    cv2.putText(color_image, f'{landmark.z:.2f}m', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+                    if recording:
+                        ROW_CSV[id*3] = x
+                        ROW_CSV[id*3+1] = y
+                        ROW_CSV[id*3+2] = landmark.z
+
+                    
+                    
+
+
+        # Visualize hands without displaying depth information
         if hand_results.multi_hand_landmarks:
             for hand_landmarks in hand_results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(color_image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-                for landmark in hand_landmarks.landmark:
-                    if 0<= landmark.x <= 1 and 0<= landmark.y <= 1:
-                        x = int(landmark.x * color_image.shape[1])
-                        y = int(landmark.y * color_image.shape[0])
-                        depth = depth_frame.get_distance(x, y)
-                        #cv2.putText(color_image, f'{depth:.2f}m', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
-        # Draw arm landmarks and connectors from pose detection
-        if pose_results.pose_landmarks:
-            right_shoulder = pose_results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER]
-            right_elbow = pose_results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_ELBOW]
-            right_wrist = pose_results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_WRIST]
-
-            arm_landmarks = [right_shoulder, right_elbow, right_wrist]
-            arm_points = []
-
-            for landmark in arm_landmarks:
-                if landmark.x >= 0 and landmark.x <= 1 and landmark.y >= 0 and landmark.y <= 1:
+                # Korrekter Aufruf von mp_drawing.draw_landmarks
+                mp_drawing.draw_landmarks(
+                    color_image, 
+                    hand_landmarks, 
+                    mp_hands.HAND_CONNECTIONS,
+                    mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=4),
+                    mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2)
+                )
+                
+                
+                for id, landmark in enumerate(hand_landmarks.landmark):
                     x = int(landmark.x * color_image.shape[1])
                     y = int(landmark.y * color_image.shape[0])
-                    depth = depth_frame.get_distance(x, y)
-                    _,_, actual_depth = norm_vector_to_other_vector(VECTOR_FOR_CALIBRATION,(x,y,depth))
-                    cv2.putText(color_image, f'{actual_depth:.2f}m', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-                    cv2.circle(color_image, (x, y), 5, (255, 0, 0), cv2.FILLED)
-                    arm_points.append((x, y))
+                    
+                    if point_in_screen(landmark.x, landmark.y):
+                            if id == mp_hands.HandLandmark.INDEX_FINGER_TIP:
+                                cv2.putText(color_image, f'{(landmark.z+ [0, WRIST_DETPH][WRIST_DETPH!=None]):.2f}m', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
-            # Draw connectors between shoulder, elbow, and wrist
-            if len(arm_points) == 3:
-                cv2.line(color_image, arm_points[0], arm_points[1], (255, 0, 0), 2)
-                cv2.line(color_image, arm_points[1], arm_points[2], (255, 0, 0), 2)
+                            if recording:
+                                ROW_CSV[id*3] = x
+                                ROW_CSV[id*3+1] = y
+                                ROW_CSV[id*3+2] = landmark.z + [0, WRIST_DETPH][WRIST_DETPH!=None]
 
-        # Display the color image with landmarks and depth values
+
+        if recording:
+            ROW_CSV[0]=FRAME
+            csv_writer.writerow(ROW_CSV)
+            ROW_CSV = ROW_CSV = [0] * (AMOUNT_OF_LANDMARKS * 3)
+        
         cv2.imshow('RealSense', color_image)
-
-        # Exit on 'q' key press
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('r'):
+            if not recording:
+                start_recording()
+            else:
+                stop_recording()
+        elif key == ord('q'):
+            if recording:
+                stop_recording()
             break
 
 finally:
-    # Stop streaming
     pipeline.stop()
     cv2.destroyAllWindows()
+    if recording:
+        csvfile.close()
--- track_whole_arm_media.py
+++ track_whole_arm_media.py
@@ -15,15 +15,24 @@ profile = pipeline.start(config)
 # Initialize MediaPipe
 mp_hands = mp.solutions.hands
 mp_pose = mp.solutions.pose
-hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.55, min_tracking_confidence=0.5)
-pose = mp_pose.Pose(min_detection_confidence=0.55, min_tracking_confidence=0.55)
+hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7, min_tracking_confidence=0.7)
+pose = mp_pose.Pose(min_detection_confidence=0.7, min_tracking_confidence=0.7)
 mp_drawing = mp.solutions.drawing_utils
+
+AMOUNT_OF_LANDMARKS = 23 # 2*arm + 20 Finger
+ROW_CSV = [0] * (AMOUNT_OF_LANDMARKS * 3)
+
+FRAME = 0
 
 def start_recording():
     global csvfile, csv_writer, recording
     csvfile = open('recorded_data.csv', 'w', newline='')
     csv_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    csv_writer.writerow(['Landmark', 'X', 'Y', 'Z', 'Depth'])
+    header = [[f'X_{id}', f'Y_{id}', f'Z_{id}'] for id in range(AMOUNT_OF_LANDMARKS)]
+    header_final = ["FRAME"]
+    for i in header:
+        header_final.extend(i)
+    csv_writer.writerow(header_final)
     recording = True
     print("Recording started.")
 
@@ -36,18 +45,11 @@ def stop_recording():
 def point_in_screen(point_x,point_y):
     return 0<= point_x <=1 and 0<= point_y <= 1
 
-def z_relative_to_absolute(z_relational_point_absolute, z_relational_point_relative, z_calculate_relative):
-    return (abs(z_relational_point_absolute/z_relational_point_relative) * z_calculate_relative)
-
 recording = False
 
-CALIBRATION_FACTOR_SHOULDER = None
-CALIBRATION_FACTOR_WRIST = None
 CALIBRATION_FACTOR_FINGER = 0
 CALIBRATION_FACTOR_FINGER_INDEX = 0
 CALIBRATION_FACTOR_FINGER_SUM = 0
-
-ROW_CSV = []
 
 try:
     while True:
@@ -56,6 +58,7 @@ try:
         color_frame = frames.get_color_frame()
         if not depth_frame or not color_frame:
             continue
+        FRAME +=1
 
         depth_image = np.asanyarray(depth_frame.get_data())
         color_image = np.asanyarray(color_frame.get_data())
@@ -65,20 +68,23 @@ try:
 
         # Visualize and record arm landmarks with depth
         if pose_results.pose_landmarks:
-            connections = [mp_pose.PoseLandmark.RIGHT_SHOULDER, mp_pose.PoseLandmark.RIGHT_ELBOW]
-            for landmark in pose_results.pose_landmarks.landmark:           
+            arm_landmarks = [mp_pose.PoseLandmark.RIGHT_SHOULDER, mp_pose.PoseLandmark.RIGHT_ELBOW]
+            for id, joint in enumerate(arm_landmarks):
+                landmark = pose_results.pose_landmarks.landmark[joint]
                 x, y = int(landmark.x * color_image.shape[1]), int(landmark.y * color_image.shape[0])
                 
                 if point_in_screen(landmark.x, landmark.y):
-                    depth1 = depth_frame.get_distance(x, y)
+                    depth = depth_frame.get_distance(x, y)
 
-                    cv2.putText(color_image, f'{depth1:.2f}m', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                    cv2.putText(color_image, f'{depth:.2f}m', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-                    ROW_CSV.append(x)
+                    if recording:
+                        ROW_CSV[id*3] = x
+                        ROW_CSV[id*3+1] = y
+                        ROW_CSV[id*3+2] = depth
 
-                if recording:
-                    csv_writer.writerow(['Pose', connection[0], start_point.x, start_point.y, start_point.z, depth1])
-                    csv_writer.writerow(['Pose', connection[1], end_point.x, end_point.y, end_point.z, depth2])
+                    
+                    
 
 
         # Visualize hands without displaying depth information
@@ -103,31 +109,37 @@ try:
                 for id, landmark in enumerate(hand_landmarks.landmark):
                     x = int(landmark.x * color_image.shape[1])
                     y = int(landmark.y * color_image.shape[0])
-                    #print(f"{CALIBRATION_FACTOR_SHOULDER=}")
-                    #print(f"{landmark.z=}")
                     
-                    if id == mp_hands.HandLandmark.INDEX_FINGER_TIP:
-                        depth= depth_frame.get_distance(x, y)
-                        z_fingertip_rel_temp = landmark.z
-                        z_fingertip_rel = z_fingertip_rel_temp
-                        z_fingertip_abs = depth
-                    if id == mp_hands.HandLandmark.WRIST:
-                        depth= depth_frame.get_distance(x, y)
-                        z_wrist_rel_temp = landmark.z
-                        z_wrist_rel = z_wrist_rel_temp
-                        z_wrist_abs = depth
-                    if CALIBRATION_FACTOR_FINGER:
-                        depth= depth_frame.get_distance(x, y)
-                        absolute_depth = CALIBRATION_FACTOR_FINGER * (z_fingertip_rel - z_wrist_rel) + z_wrist_abs
+                    if point_in_screen(landmark.x, landmark.y):
                         if id == mp_hands.HandLandmark.INDEX_FINGER_TIP:
-                            cv2.putText(color_image, f'{absolute_depth:.2f}m', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                            z_fingertip_rel_temp = landmark.z
+                            z_fingertip_rel = z_fingertip_rel_temp
+                            z_fingertip_abs = depth_frame.get_distance(x, y)
+                        if id == mp_hands.HandLandmark.WRIST:
+                            z_wrist_rel_temp = landmark.z
+                            z_wrist_rel = z_wrist_rel_temp
+                            z_wrist_abs = depth_frame.get_distance(x, y)
+                        if CALIBRATION_FACTOR_FINGER:
+                            depth= depth_frame.get_distance(x, y)
+                            absolute_depth = CALIBRATION_FACTOR_FINGER * (z_fingertip_rel - z_wrist_rel) + z_wrist_abs
+                            if recording:
+                                print(6 + id*3, id, len(ROW_CSV))
+                                ROW_CSV[6 + id*3] = x
+                                ROW_CSV[6 + id*3+1] = y
+                                ROW_CSV[6 + id*3+2] = depth
+                            if id == mp_hands.HandLandmark.INDEX_FINGER_TIP:
+                                cv2.putText(color_image, f'{absolute_depth:.2f}m', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
                 
                 if z_fingertip_rel and z_wrist_rel and z_fingertip_abs and z_wrist_abs:
                     CALIBRATION_FACTOR_FINGER_SUM += abs(z_fingertip_abs - z_wrist_abs) / abs(z_fingertip_rel - z_wrist_rel)
                     CALIBRATION_FACTOR_FINGER_INDEX += 1
                     CALIBRATION_FACTOR_FINGER = CALIBRATION_FACTOR_FINGER_SUM / CALIBRATION_FACTOR_FINGER_INDEX
-                    print(CALIBRATION_FACTOR_FINGER)
+                    #print(CALIBRATION_FACTOR_FINGER)
 
+        if recording:
+            ROW_CSV[0]=FRAME
+            csv_writer.writerow(ROW_CSV)
+            ROW_CSV = ROW_CSV = [0] * (AMOUNT_OF_LANDMARKS * 3)
         
         cv2.imshow('RealSense', color_image)
         key = cv2.waitKey(1) & 0xFF
@@ -137,8 +149,6 @@ try:
             else:
                 stop_recording()
         elif key == ord('q'):
-            df = pd.DataFrame(abs_and_rel, columns=['Depth', 'Landmark.z', 'Ratio'])
-            df.to_csv('abs_to_rel.csv', index=False)
             if recording:
                 stop_recording()
             break
